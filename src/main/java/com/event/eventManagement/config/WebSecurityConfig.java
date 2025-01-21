d6c850078975be23c48b1897eb4d57bebf6a8a89
Rahaf Perween <rahafperween@gmail.com>
2025-01-22 01:13:24 +0530

    Authorization partial work

---
+++ src/main/java/com/event/eventManagement/config/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.event.eventManagement.config;
+
+import com.event.eventManagement.security.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Autowired
+    private CustomUserDetailService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.cors(Customizer.withDefaults())
+                //comment the below line to disable csrf protection against POST, PUT, DELETE requests
+                .csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(request->request.anyRequest().authenticated())
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+   public AuthenticationProvider authenticationProvider(){
+       DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+       daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+       daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+       return daoAuthenticationProvider;
+   }
+
+   @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+   }
+}
---
+++ src/main/java/com/event/eventManagement/controller/AuthController.java
@@ -0,0 +1,8 @@
+package com.event.eventManagement.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+}
